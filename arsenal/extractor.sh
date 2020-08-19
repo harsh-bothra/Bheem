f7c5421dc06bcec534584828a4a560ac2a60ab35
Harsh Bothra <66528650+harsh-bothra@users.noreply.github.com>
2020-08-19 21:08:11 +0530

    Updated Regex
    Updated Regex

--- arsenal/extractor.sh
+++ arsenal/extractor.sh
@@ -7,7 +7,7 @@ gf xss $dir/$1_final_urls | cut -d : -f3- | sort -u > $dir/$1_xss;
 gf ssti $dir/$1_final_urls | sort -u > $dir/$1_ssti
 gf ssrf $dir/$1_final_urls | sort -u > $dir/$1_ssrf
 gf sqli $dir/$1_final_urls | sort -u > $dir/$1_sqli
-gf redirect  $dir/$1_final_urls | cut -d : -f3- | sort -u > $dir/$1_redirect
+gf redirect  $dir/$1_final_urls | cut -d : -f2- | sort -u > $dir/$1_redirect
 gf rce  $dir/$1_final_urls | sort -u > $dir/$1_rce
 gf potential $dir/$1_final_urls| cut -d : -f3- | sort -u > $dir/$1_potential
 gf lfi  $dir/$1_final_urls | sort -u > $dir/$1_lfi
