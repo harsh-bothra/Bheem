20b0d95d1e8c8f7da234b868fc47d5f8bd3dfd41
Kathan Patel <patelkathan22@gmail.com>
2020-08-24 20:47:08 +0530

    Fixed Path Issue
    Fixed Path Issue for smuggler.py

--- arsenal/smuggler.sh
+++ arsenal/smuggler.sh
@@ -4,4 +4,4 @@ dir=/root/Recon/$1
 
 mkdir $dir
 
-echo https://$1 | python3 /root/Tools/smuggler.py > $dir/$1_smuggler
+echo https://$1 | python3 /root/Tools/smuggler/smuggler.py > $dir/$1_smuggler
