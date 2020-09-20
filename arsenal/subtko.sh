be9bc29dc9bb21e3d48518a9a541425806b3d592
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-09-20 22:41:17 +0530

    Update subtko.sh

--- arsenal/subtko.sh
+++ arsenal/subtko.sh
@@ -2,4 +2,5 @@
 
 dir=~/Recon/$1
 
+[[ -f ~/go/src/github.com/manasmbellani/subjack/fingerprints.json ]] || wget -q -O ~/go/src/github.com/manasmbellani/subjack/fingerprints.json https://raw.githubusercontent.com/manasmbellani/subjack/master/fingerprints.json
 subjack -w $dir/$1_subdomains -timeout 30 -ssl -c ~/go/src/github.com/manasmbellani/subjack/fingerprints.json -v -m -o $dir/result_subdomain_takeover.txt
