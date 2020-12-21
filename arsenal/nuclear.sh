41b354e9e9bc38501cd25ce14a039a91806c0afb
shubham chaskar <48474764+unstabl3@users.noreply.github.com>
2020-12-21 18:59:36 +0530

    added custom header to nuclei scan
    Hi,
    Sometimes websites block and/or gives different results if we run nuclei without a proper header!
    the default header that goes with every request is (Nuclei - Open-source project (github.com/projectdiscovery/nuclei)
    
    it's good to have a custom header rather than a default one to bypass protections at some level.
    Hope that helps!
    Best,
    Shubham

--- arsenal/nuclear.sh
+++ arsenal/nuclear.sh
@@ -2,5 +2,5 @@
 
 dir=~/Recon/$1
 
-cat $dir/$1_probed | ~/go/bin/nuclei -c 500 -silent -t ~/Tools/nuclei-templates/ -o $dir/$1_nuclei;
+cat $dir/$1_probed | ~/go/bin/nuclei -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -c 500 -silent -t ~/Tools/nuclei-templates/ -o $dir/$1_nuclei;
 
